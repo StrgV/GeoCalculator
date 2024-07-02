b591ff1915030659566c9e5e88e99578c87e6c4f
Tim Porger <timporgertim@gmail.com>
2024-07-02 13:51:59 +0200

    added cool inhaltsverzeichnis ye

--- selector.js
+++ selector.js
@@ -4,6 +4,10 @@ function markBordersInColor(elements, color){
     });
 }
 
+function markSectionBorders(element, color){
+    element.style.border = '3px solid red';
+}
+
 function clearMarkings(){
     var elements = document.querySelectorAll('div.eq_Title');
     markBordersInColor(elements, "hsl(280, 76%, 65%)");
@@ -11,6 +15,10 @@ function clearMarkings(){
     markBordersInColor(elements, "hsl(280, 76%, 65%)");
     var elements = document.querySelectorAll('div.eq_Image');
     markBordersInColor(elements, "hsl(280, 76%, 65%)");
+    var element = document.getElementById('2dGrid');
+    element.style.border = 'none';
+    var element = document.getElementById('3dGrid');
+    element.style.border = 'none';
 }
 
 function markRechteck(){
@@ -33,3 +41,32 @@ function markViereck(){
     markBordersInColor(elements, "red");
 }
 
+function markWuerfel(){
+    var elements = document.querySelectorAll('div.wuerfel');
+    markBordersInColor(elements, "red");
+}
+
+function markQuader(){
+    var elements = document.querySelectorAll('div.quader');
+    markBordersInColor(elements, "red");
+}
+
+function markPyramide(){
+    var elements = document.querySelectorAll('div.pyramide');
+    markBordersInColor(elements, "red");
+}
+
+function markKugel(){
+    var elements = document.querySelectorAll('div.kugel');
+    markBordersInColor(elements, "red");
+}
+
+function mark2D(){
+    var element = document.getElementById('2dGrid');
+    markSectionBorders(element)
+}
+
+function mark3D(){
+    var element = document.getElementById('3dGrid');
+    markSectionBorders(element);
+}
